f2837cc1419f88ebe4c41805ec15868fae9dddbf
Junichi Fujiwara <fujiwarajunichi2000@outlook.jp>
2022-02-22 17:21:52 +0900

    service workers

--- plugin.js
+++ plugin.js
@@ -13,10 +13,31 @@ exports.hooks={
     let base=`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
     urls.unshift("");
     base+=urls.map(e=>`<url>\n<loc>https://ngeojiajun.github.io/${e}</loc>\n</url>`).join("\n");
-    base+="\n</urlset>";
-    fs.writeFileSync("./out/sitemap.xml",base);
-    //txt version
-    let final=urls.map(e=>`https://ngeojiajun.github.io/${e}`).join("\n");
-    fs.writeFileSync("./out/sitemap.txt",final);
+      base+="\n</urlset>";
+      fs.writeFileSync("./out/sitemap.xml",base);
+      //txt version
+      let final=urls.map(e=>`https://ngeojiajun.github.io/${e}`).join("\n");
+        fs.writeFileSync("./out/sitemap.txt",final);
+      }
+}
+//get effective commit of the tree
+//since the tree always clean in CI so we can ignore the fact that the tree might be
+//modifed
+exports.git_commit=function(){
+  //first check if the .git exists
+  if(!fs.existsSync(".git")){
+    //the PWD is not git repo
+    return "0000000000000000000000000000000000000000";
+  }
+  //second get its ref
+  //ref can be either the commit id or the ref to branch
+  const ref=fs.readFileSync(".git/HEAD",'utf8').split(":").slice(-1)[0].trim();
+  if(ref.includes("/")){
+    //external reference to branch
+    return fs.readFileSync(`.git/${ref}`,'utf8').trim();
+  }
+  else{
+    //checked out on a commit id
+    return ref;
   }
 }
